6470210584cb55e06cbbc2ba12e5229d2cd0d856
Bitnik212 <Bit@DESKTOP-L8PS63V>
2020-11-28 23:19:59 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Xml;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class server
+    {
+        string url = "http://partner.market.yandex.ru/pages/help/YML.xml";
+        HttpClient client = new HttpClient();
+        XmlDocument xmlres = new XmlDocument();
+
+
+        public async Task getProducts()
+        {
+            var res = await client.GetAsync(new Uri(this.url));
+            xmlres.LoadXml(res.Content.ReadAsStringAsync().Result);
+            Debug.WriteLine(xmlres);
+        }
+    }
+}
